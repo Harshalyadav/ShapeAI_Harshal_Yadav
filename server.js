b1fec1352325f35fb3c21062ea1ad09b13c2e795
Harshu <54142355+Harshalyadav@users.noreply.github.com>
2021-05-17 18:42:17 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyparser=require("body-parser");
+
+const app = express();
+app.use(bodyparser.urlencoded({extended: true}));
+app.get("/",function(req,res){
+
+    res.sendFile(__dirname+"/index.html");
+});
+
+app.post("/",function(req,res){
+   
+    var n1 = Number(req.body.num1);
+    var n2 = Number(req.body.num2);
+    var result = n1+n2;
+
+    res.send("Your BMI is " + result);
+});
+
+app.listen(3000,function(){
+console.log("HELLO WORLD");
+
+});
